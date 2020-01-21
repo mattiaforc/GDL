3d0c50e04bd0e37a8d8d06255e8652921e50c537
MalattiaForcellese <mattiaforc@gmail.com>
2020-01-21 21:12:32 +0100

    Shortest path adjacency matrix

--- G2G/preprocess/generate.py
+++ G2G/preprocess/generate.py
@@ -5,13 +5,13 @@ from typing import List, Tuple, Dict, Generator
 
 
 class GraphWrapper:
-    def __init__(self, adj: torch.Tensor):
+    def __init__(self, adj: torch.Tensor, pos=None):
         self.adj = adj
         edges, self.labels = get_labeled_edges(self.adj)
         self.graph = nx.Graph()
         self.graph.add_edges_from(edges)
         nx.set_edge_attributes(self.graph, {k: float(v) for k, v in self.labels.items()}, name='weight')
-        self.pos = nx.spring_layout(self.graph)
+        self.pos = pos or nx.spring_layout(self.graph)
 
     def print(self):
         print(self.adj)
@@ -29,7 +29,7 @@ def generate_graphs(iterations: int, N: int) -> Generator[GraphWrapper, None, No
             for j in range(i + 1):
                 A[i][j] = A[j][i]
             else:
-                A[i][j + 1::] = torch.randint(3, (1, N - i - 1))
+                A[i][j + 1::] = torch.nn.functional.relu(torch.randn((1, N - i - 1)))
         yield GraphWrapper(A)
 
 
@@ -37,18 +37,30 @@ def get_labeled_edges(g: torch.Tensor) -> Tuple[List[Tuple[int, int]], Dict[Tupl
     edges: List[Tuple[int, int]] = []
     labels: Dict[Tuple[int, int], str] = {}
     for i in range(g.shape[0]):
-        for j in range(i, g.shape[0]):
+        for j in range(g.shape[0]):
             if g[i][j] != 0:
                 edges.extend([(i + 1, j + 1), (j + 1, i + 1)])
-                labels[(i + 1, j + 1)] = str(g[i][j].item())
+                labels[(i + 1, j + 1)] = str(g[i][j].item())[0:4]
     return edges, labels
+
+
+def shortest_path_as_adjacency_matrix(g: GraphWrapper, start: int, end: int) -> torch.Tensor:
+    A = torch.zeros(g.adj.shape)
+    try:
+        shortest_path = nx.shortest_path(g.graph, start, end, weight="weight")
+        for s, e in zip(shortest_path, shortest_path[1:]):
+            A[s - 1][e - 1] = g.adj[s - 1][e - 1]
+    except nx.NetworkXNoPath:
+        pass
+    return A
 
 
 def main():
     for graph in generate_graphs(1, 7):
         graph.print()
-        # Raise exception
-        print(nx.shortest_path(graph.graph, 1, 7, weight="weight"))
+        s = GraphWrapper(shortest_path_as_adjacency_matrix(graph, 1, 6), pos=graph.pos)
+        print(nx.shortest_path(graph.graph, 1, 6, weight="weight"))
+        s.print()
 
 
 if __name__ == "__main__":
