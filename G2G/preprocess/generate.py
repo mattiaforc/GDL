b8c393068e385835f7b0ca4a3bbf633f4df56285
MalattiaForcellese <mattiaforc@gmail.com>
2020-01-21 12:36:07 +0100

    Refactor graph class + printing

--- G2G/preprocess/generate.py
+++ G2G/preprocess/generate.py
@@ -1,10 +1,27 @@
 import torch
 import networkx as nx
 import matplotlib.pyplot as plt
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Generator
 
 
-def generate_graphs(iterations: int, N: int) -> torch.Tensor:
+class GraphWrapper:
+    def __init__(self, adj: torch.Tensor):
+        print(adj)
+        self.adj = adj
+        edges, self.labels = get_labeled_edges(self.adj)
+        self.graph = nx.Graph()
+        self.graph.add_edges_from(edges)
+        self.pos = nx.spring_layout(self.graph)
+
+    def print(self):
+        nx.draw_networkx_edge_labels(self.graph, self.pos, self.labels)
+        nx.draw_networkx_nodes(self.graph, self.pos)
+        nx.draw_networkx_edges(self.graph, self.pos)
+        nx.draw_networkx_labels(self.graph, self.pos)
+        plt.show()
+
+
+def generate_graphs(iterations: int, N: int) -> Generator[GraphWrapper, None, None]:
     for _ in range(iterations):
         A = torch.zeros((N, N))
         for i in range(N):
@@ -12,32 +29,23 @@ def generate_graphs(iterations: int, N: int) -> torch.Tensor:
                 A[i][j] = A[j][i]
             else:
                 A[i][j + 1::] = torch.randint(6, (1, N - i - 1))
-        yield A
+        yield GraphWrapper(A)
 
 
-def get_labeled_edges_as_tuple(g: torch.Tensor) -> Tuple[List[Tuple[int, int]], Dict[Tuple[int, int], str]]:
-    tuples: List[Tuple[int, int]] = []
+def get_labeled_edges(g: torch.Tensor) -> Tuple[List[Tuple[int, int]], Dict[Tuple[int, int], str]]:
+    edges: List[Tuple[int, int]] = []
     labels: Dict[Tuple[int, int], str] = {}
     for i in range(g.shape[0]):
         for j in range(i, g.shape[0]):
             if g[i][j] != 0:
-                tuples.extend([(i + 1, j + 1), (j + 1, i + 1)])
+                edges.extend([(i + 1, j + 1), (j + 1, i + 1)])
                 labels[(i + 1, j + 1)] = str(g[i][j].item())
-    return tuples, labels
+    return edges, labels
 
 
 def main():
-    for graph in generate_graphs(1, 5):
-        print(graph)
-        pg = nx.Graph()
-        tuples, labels = get_labeled_edges_as_tuple(graph)
-        pg.add_edges_from(tuples)
-        pos = nx.spring_layout(pg)
-        nx.draw_networkx_edge_labels(pg, pos, labels)
-        nx.draw_networkx_nodes(pg, pos)
-        nx.draw_networkx_edges(pg, pos)
-        nx.draw_networkx_labels(pg, pos)
-        plt.show()
+    for graph in generate_graphs(2, 5):
+        graph.print()
 
 
 if __name__ == "__main__":
